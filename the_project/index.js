f2a753f3ac09abcab1379a25d6b9d757e8c28dfb
OlaviKom <antti.o.komulainen@gmail.com>
2025-12-22 11:42:20 +0200

    1.2

---
+++ the_project/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from todo app</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
