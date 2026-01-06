2a94f938ed56d67926a6077035a530f5f731a895
OlaviKom <antti.o.komulainen@gmail.com>
2026-01-07 01:51:33 +0200

    1.12

--- the_project/README.md
+++ the_project/README.md
@@ -1,5 +1,7 @@
 # Todo App
 
+For the PersistentVolume to work you first need to create the local path in the node we are binding it to. create a directory at `/tmp/kube` in the container `k3d-k3s-default-agent-0`. This can simply be done via `docker exec k3d-k3s-default-agent-0 mkdir -p /tmp/kube`
+
 Navigate to app `cd path/to/log_output`
 
 Deploy with `kubectl apply -f manifests/`
--- the_project/index.js
+++ the_project/index.js
@@ -1,11 +1,41 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const axios = require("axios");
+const fsPromises = require("fs/promises");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 8080;
+const IMAGE_URL = "https://picsum.photos/1200";
+const IMAGE_DIR = "/app/data";
+const IMAGE_PATH = path.join(IMAGE_DIR, "image.jpg");
+const TIMELIMIT = 10 * 60 * 1000;
 
-app.get("/", (req, res) => {
+const isImageCacheValid = async () => {
+  try {
+    const stats = await fsPromises.stat(IMAGE_PATH);
+    const imageAge = Date.now() - stats.mtimeMs;
+    return imageAge < TIMELIMIT;
+  } catch (err) {
+    if (err.code === "ENOENT") return false;
+    throw err;
+  }
+};
+
+const fetchImage = async () => {
+  await fsPromises.mkdir(IMAGE_DIR, { recursive: true });
+  if (await isImageCacheValid()) return;
+  const res = await axios.get(IMAGE_URL, { responseType: "stream" });
+  res.data.pipe(fs.createWriteStream(IMAGE_PATH));
+};
+
+app.get("/", async (req, res) => {
+  await fetchImage();
   res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get("/image", async (req, res) => {
+  res.sendFile(IMAGE_PATH);
 });
 
 app.listen(PORT, () => {
