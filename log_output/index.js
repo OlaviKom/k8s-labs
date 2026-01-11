b74be5b98aa069384a88b459ce3fcc319e9b4f1d
OlaviKom <antti.o.komulainen@gmail.com>
2026-01-11 23:33:21 +0200

    2.5

--- log_output/index.js
+++ log_output/index.js
@@ -6,18 +6,22 @@ const { randomUUID } = require("crypto");
 
 const PORT = process.env.PORT || 8080;
 const PINGPONG_URL = "http://ping-pong-svc:2450/pingpong";
+const CONFIG_FILE = "/config/information.txt";
 
 const randomString = randomUUID();
 
 app.get("/", async (req, res) => {
   const response = await axios.get(PINGPONG_URL);
   const pingpongCount = response.data.split(" ")[1];
+  const fileContent = await fs.readFile(CONFIG_FILE);
+  const envVariable = process.env.MESSAGE;
 
   const timestamp = new Date().toISOString();
 
-  res
-    .type("text/plain")
-    .send(`${timestamp}: ${randomString}\nPing / Pongs: ${pingpongCount}`);
+  res.type("text/plain").send(`file content: ${fileContent}\n
+env variable: ${envVariable}\n
+${timestamp}: ${randomString}\n
+Ping / Pongs: ${pingpongCount}`);
 });
 
 app.listen(PORT, () => {
