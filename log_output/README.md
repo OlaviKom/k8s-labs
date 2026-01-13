1d4b8fe0048d6f8da4e7cea0f98e85b3e2bacb28
OlaviKom <antti.o.komulainen@gmail.com>
2026-01-13 13:33:27 +0200

    2.7

--- log_output/README.md
+++ log_output/README.md
@@ -1,9 +1,13 @@
 # Log output app
 
-The application shows timestamp, random string and how many request is done to /pingpong
+The application shows timestamp, random string and how many request is done to /pingpong. Count of request is saved on Postgres database.
 
 Create namespace `kubectl create namespace exercises`
 
+Use namespace `kubectl config set-context --current --namespace=exercises`
+
 Navigate to app `cd path/to/log_output`
+
+create secret.yaml file to the manifest dir `touch manifests/secret.yaml` . Use `manifests/secret.yaml.example` as a template. Remember format values to base64 format. You can example do formating with command: `echo -n 'our value' | base64`
 
 Deploy with `kubectl apply -f manifests/`
--- ping-pong/index.js
+++ ping-pong/index.js
@@ -1,16 +1,58 @@
 const express = require("express");
 const app = express();
-const fs = require("fs/promises");
+const { Client } = require("pg");
 
 const PORT = process.env.PORT || 8080;
 
-var counter = 0;
+const client = new Client({
+  host: process.env.DB_HOST,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  port: Number(process.env.DB_PORT),
+});
+
+const initDB = async () => {
+  await client.connect();
+
+  await client.query(`
+    CREATE TABLE IF NOT EXISTS pingpong(
+    id INTEGER PRIMARY KEY,
+    value INTEGER NOT NULL
+    )
+  `);
+
+  await client.query(`
+    INSERT INTO pingpong (id, value)
+    VALUES (1, 0)
+    ON CONFLICT (id) DO NOTHING`);
+};
 
 app.get("/pingpong", async (req, res) => {
-  counter++;
-  res.send(`pong ${counter}`);
+  try {
+    const result = await client.query(`
+    UPDATE pingpong
+    SET value = value + 1
+    WHERE id = 1
+    RETURNING value`);
+    res.send(`pong ${result.rows[0].value}`);
+  } catch (err) {
+    console.error("Query failed", err);
+    res.status(500).send("Database error");
+  }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const start = async () => {
+  try {
+    await initDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
+  } catch (err) {
+    console.error("DB init failed", err);
+    process.exit(1);
+  }
+};
+
+start();
