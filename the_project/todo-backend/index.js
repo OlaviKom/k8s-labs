5dc57ab239ca6bbbf44ae68e7090dabdc4838d09
OlaviKom <antti.o.komulainen@gmail.com>
2026-01-29 23:18:53 +0200

    4.2 test probe without database access

--- the_project/todo-backend/index.js
+++ the_project/todo-backend/index.js
@@ -84,14 +84,12 @@ app.get("/livez", (req, res) => {
 const start = async () => {
   try {
     await initDB();
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
   } catch (err) {
     console.error("DB init failed", err);
-    process.exit(1);
   }
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 };
 
 start();
